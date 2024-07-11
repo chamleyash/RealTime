3b6ea91e7806c165fa6fe66e2aae551bbcf50fbc
LzyProgrammr <chamleyash@gmail.com>
2024-07-11 10:36:13 +0530

    update changes

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Team, Teammate
+from .forms import TeamForm
 
-# Register your models here.
+class TeammateInline(admin.TabularInline):
+    model = Teammate
+    extra = 1
+
+class TeamAdmin(admin.ModelAdmin):
+    inlines = [TeammateInline]
+
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Teammate)
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .forms import TeamForm
-from .models import Teammate
+from .forms import TeamForm, TeammateForm
+from .models import Team, Teammate
 
 def index(request):
     return render(request, 'index.html')
@@ -8,19 +8,32 @@ def index(request):
 def tournament(request):
     return render(request, 'Tournament.html')
 
+from django.shortcuts import render, redirect
+from .forms import TeamForm
+from .models import Team, Teammate
+
 def register(request):
     if request.method == 'POST':
-        team_form = TeamForm(request.POST)
-        if team_form.is_valid():
-            team = team_form.save()
-            for i in range(1, 6):
-                teammate_name = request.POST.get(f'teammate{i}Name')
-                if teammate_name:
-                    Teammate.objects.create(team=team, name=teammate_name)
-            return redirect('success')
-    else:
-        team_form = TeamForm()
-    return render(request, 'register.html', {'team_form': team_form})
+        print(request.POST)  # Debugging line
 
+        team_leader_name = request.POST.get('teamLeaderName')
+        team_leader_email = request.POST.get('teamLeaderEmail')
+        team_leader_id = request.POST.get('teamLeaderID')
+
+        team = Team(
+            team_name=team_leader_name,  # You might want to change this if team name is different
+            team_leader_name=team_leader_name,
+            team_leader_email=team_leader_email,
+            team_leader_id=team_leader_id
+        )
+        team.save()
+
+        for i in range(1, 6):  # Assuming a maximum of 5 teammates
+            teammate_name = request.POST.get(f'teammate{i}Name')
+            if teammate_name:
+                Teammate.objects.create(team=team, name=teammate_name)
+
+        return redirect('success')  # Change this to your success URL name or view
+    return render(request, 'register.html')
 def success(request):
     return render(request, 'success.html')
